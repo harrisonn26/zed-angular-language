0e8abfa524008d67da511c5306829f5e909adc7a
Harrison Norris <harrisonnorris@Harrisons-MacBook-Air.local>
2026-01-28 23:57:57 +1100

    Update outline.scm

--- languages/html/outline.scm
+++ languages/html/outline.scm
@@ -3,3 +3,21 @@
 (element
   (start_tag
     (tag_name) @name)) @item
+
+(if_statement
+    (control_keyword) @name) @item
+
+(for_statement
+    (control_keyword) @name) @item
+
+(else_statement
+    (control_keyword) @name) @item
+
+(switch_statement
+    (control_keyword) @name) @item
+
+(case_statement
+    (control_keyword) @name) @item
+
+(let_statement
+    (control_keyword) @name) @item
